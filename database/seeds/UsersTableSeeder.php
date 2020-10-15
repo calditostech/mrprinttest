793a851d296a2697f97300be8b5d7bac7efee3e4
Ricardo Moreira <calditos1988@outlook.com>
2020-10-15 02:11:55 -0300

    criação da seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,33 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
-
-	static $users = [
-        'Bento',
-        'Bernado',
-        'Cicero',
-        'Davi',
-        'Helton',
-        'Lucio',
-        'Mauricio',
-        'Nair',
-        'Osnir',
-        'Rildo',
-        'Tonio'
-    ];
-
-    static $emails = [
-        'calditos1988@outlook.com',
-        'rmoreira787@gmail.com'
-    ];
-
-    static $passwords = [
-         '1234',
-         '123'
-    ];
     /**
      * Run the database seeds.
      *
@@ -35,23 +12,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        foreach (self::$users as $user) {
-            DB::table('users')->insert([
-                'name' => $user,
-            ]);
-        }
-
-
-        foreach (self::$emails as $email) {
-            DB::table('users')->insert([
-                'email' => $email,
-            ]);
-        }
-
-        foreach (self::$passwords as $password) {
-            DB::table('users')->insert([
-                'password' => $password,
-            ]);
-        }
+        User::create([
+          "name"     => 'Ricardo Moreira',
+          "email"    => 'ricardo@teste.com.br',
+          "password" =>  bcrypt('ricardo'),
+        ]);
     }
 }
